0f6de32e720ea52d56d71c68b927b66a8f5ad59e
Edmond Leahy <Erleahy@ucalgary.ca>
2020-09-08 18:05:45 -0600

    Adding better way to have library

--- SpoofedNMEA/Nmea_spoofing/test_spoof_log/test_spoof_log.ino
+++ SpoofedNMEA/Nmea_spoofing/test_spoof_log/test_spoof_log.ino
@@ -1,32 +1,35 @@
-#include <Nmea_spoofing.h>
+#include "Nmea_spoof_internal.h"
 #include <SoftwareSerial.h>
-
-char* nmea_test = "$GPGGA,201245.000,5106.9388,N,11410.5665,W,2,10,0.86,1156.3,M,-17.5,M,0000,0000*61";
+String nmea_test = "$GPGGA,201245.000,5106.9388,N,11410.5665,W,2,10,0.86,1156.3,M,-17.5,M,0000,0000*61";
 
 void setup(){
   Serial.begin(115200);
-  delay(1000);
+//  delay(1000);
   
   
 }
 
 
 void loop(){
-  char* a = "";
-//  //a = spoof_nmea("This is a , comma, right");
+  String a = "This is a , comma, right";
+  Serial.println(find_location_of_lat(nmea_test));
+  
+//  Serial.println("spoofing");
+//  const char* spoofed = spoof_nmea(a);
+//  Serial.println("spoofed");
 //  Serial.println("Start iteration");
-  a = spoof_nmea(nmea_test);
-  delay(10);
-  Serial.println("Should print!");
-  Serial.println(a);
+//  a = spoof_nmea(nmea_test);
+  delay(1000);
+//  Serial.println(spoofed);
+//  Serial.println(a);
 //  delay(10);
 //  Serial.println("Finish iteration");
 //  delay(10);
-  Serial.println("");
-  Serial.println("");
-  Serial.println("");
-  Serial.println("");
-  delay(2000);
+//  Serial.println("");
+//  Serial.println("");
+//  Serial.println("");
+//  Serial.println("");
+//  delay(2000);
 }
 
 
